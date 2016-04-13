616be2057f735371fa9e8d43b8575a17f8f91d2d
FusionLord <FusionLord@live.com>
2016-04-13 11:12:37 -0700

    updated

--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,7 @@ module.exports.loop = function () {
 	populate.run();
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];
-    	if (name.startsWith("Harvester")) { harvester.run(creep); }
+    	if (name.startsWith("Harvester") || Game.creeps.length == 1) { harvester.run(creep); }
     	if (name.startsWith("Upgrader")) { upgrader.run(creep); }
     	if (name.startsWith("Builder")) { builder.run(creep); }
     }
