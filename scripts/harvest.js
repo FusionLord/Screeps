3332b71c2c4019d5f001b34825e1267004f34a51
FusionLord <FusionLord@live.com>
2016-04-13 11:24:04 -0700

    updated

--- scripts/harvest.js
+++ scripts/harvest.js
@@ -1,7 +1,7 @@
 module.exports = {
     run(creep) {
         if(creep.carry.energy < creep.carryCapacity) {
-            var source = creep.room.findClosestByRange(FIND_SOURCES);
+            var source = creep.room.findNearest(SOURCES);
             if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source);
             }
