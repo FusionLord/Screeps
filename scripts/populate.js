0e317ced1dba67be73b45db950ae492a9b926b2a
FusionLord <FusionLord@live.com>
2016-04-13 10:58:06 -0700

    updated

--- scripts/populate.js
+++ scripts/populate.js
@@ -6,17 +6,17 @@ module.exports = {
 		if (spawn.energy > 200) {
 			var count = _.filter(Game.creeps, function(creep){ return creep.name.startsWith('Harvester') && creep.ticksToLive > 12; }).length
 			if (count < 5) {
-                Memory.harvesters.count += 1;
+                Memory.harvesters.count = Memory.harvesters.count + 1;
 				spawn.createCreep([WORK, CARRY, MOVE], 'Harvester' + Memory.harvesters.count);
 			}
 			count = _.filter(Game.creeps, function(creep){ return creep.name.startsWith('Builder') && creep.ticksToLive > 12; }).length
 			if (count < 5) {
-                Memory.builders.count += 1;
+                Memory.builders.count = Memory.builders.count + 1;
 				spawn.createCreep([WORK, CARRY, MOVE], 'Builder' + Memory.builders.count);
 			}
 			count = _.filter(Game.creeps, function(creep){ return creep.name.startsWith('Upgrader') && creep.ticksToLive > 12; }).length
 			if (count < 2) {
-                Memory.upgraders.count += 1;
+                Memory.upgraders.count = Memory.upgraders.count + 1;
 				spawn.createCreep([WORK, CARRY, MOVE], 'Upgrader' + Memory.upgraders.count);
 			}
 		}
