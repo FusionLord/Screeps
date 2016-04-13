e24cebf246ea1d1ea6a5e7543e19a62ca92f96b7
FusionLord <FusionLord@live.com>
2016-04-13 10:59:29 -0700

    updated

--- scripts/populate.js
+++ scripts/populate.js
@@ -6,18 +6,18 @@ module.exports = {
 		if (spawn.energy > 200) {
 			var count = _.filter(Game.creeps, function(creep){ return creep.name.startsWith('Harvester') && creep.ticksToLive > 12; }).length
 			if (count < 5) {
-                Memory.harvesters.count = Memory.harvesters.count + 1;
-				spawn.createCreep([WORK, CARRY, MOVE], 'Harvester' + Memory.harvesters.count);
+                Memory.harvestersCount = Memory.harvestersCount + 1;
+				spawn.createCreep([WORK, CARRY, MOVE], 'Harvester' + Memory.harvestersCount);
 			}
 			count = _.filter(Game.creeps, function(creep){ return creep.name.startsWith('Builder') && creep.ticksToLive > 12; }).length
 			if (count < 5) {
-                Memory.builders.count = Memory.builders.count + 1;
-				spawn.createCreep([WORK, CARRY, MOVE], 'Builder' + Memory.builders.count);
+                Memory.buildersCount = Memory.buildersCount + 1;
+				spawn.createCreep([WORK, CARRY, MOVE], 'Builder' + Memory.buildersCount);
 			}
 			count = _.filter(Game.creeps, function(creep){ return creep.name.startsWith('Upgrader') && creep.ticksToLive > 12; }).length
 			if (count < 2) {
-                Memory.upgraders.count = Memory.upgraders.count + 1;
-				spawn.createCreep([WORK, CARRY, MOVE], 'Upgrader' + Memory.upgraders.count);
+                Memory.upgradersCount = Memory.upgradersCount + 1;
+				spawn.createCreep([WORK, CARRY, MOVE], 'Upgrader' + Memory.upgradersCount);
 			}
 		}
     }
