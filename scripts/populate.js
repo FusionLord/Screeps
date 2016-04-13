811135c8a3af21195cc7f7556a3887443cfc17b9
FusionLord <FusionLord@live.com>
2016-04-13 11:11:22 -0700

    updated

--- scripts/populate.js
+++ scripts/populate.js
@@ -2,23 +2,33 @@ var _ = require('lodash');
 
 module.exports = {
     run() {
-		var spawn = Game.spawns.Spawn1;
-		if (spawn.energy > 200) {
-			var count = _.filter(Game.creeps, function(creep){ return creep.name.startsWith('Harvester') && creep.ticksToLive > 12; }).length
-			if (count < 5) {
+        var spawn = Game.spawns.Spawn1;
+        if (spawn.energy > 200) {
+            var count = _.filter(Game.creeps, function (creep) {
+                return creep.name.startsWith('Harvester') && creep.ticksToLive > 12;
+            }).length;
+            if (count < 5) {
                 Memory.harvestersCount = Memory.harvestersCount + 1;
-				spawn.createCreep([WORK, CARRY, MOVE], 'Harvester' + Memory.harvestersCount);
-			}
-			count = _.filter(Game.creeps, function(creep){ return creep.name.startsWith('Builder') && creep.ticksToLive > 12; }).length
-			if (count < 5) {
-                Memory.buildersCount = Memory.buildersCount + 1;
-				spawn.createCreep([WORK, CARRY, MOVE], 'Builder' + Memory.buildersCount);
-			}
-			count = _.filter(Game.creeps, function(creep){ return creep.name.startsWith('Upgrader') && creep.ticksToLive > 12; }).length
-			if (count < 2) {
-                Memory.upgradersCount = Memory.upgradersCount + 1;
-				spawn.createCreep([WORK, CARRY, MOVE], 'Upgrader' + Memory.upgradersCount);
-			}
-		}
+                spawn.createCreep([WORK, CARRY, MOVE], 'Harvester' + Memory.harvestersCount);
+            }
+            if (count >= 5) {
+                count = _.filter(Game.creeps, function (creep) {
+                    return creep.name.startsWith('Builder') && creep.ticksToLive > 12;
+                }).length;
+                if (count < 5) {
+                    Memory.buildersCount = Memory.buildersCount + 1;
+                    spawn.createCreep([WORK, CARRY, MOVE], 'Builder' + Memory.buildersCount);
+                }
+                if (count >= 5) {
+                    count = _.filter(Game.creeps, function (creep) {
+                        return creep.name.startsWith('Upgrader') && creep.ticksToLive > 12;
+                    }).length;
+                    if (count < 2) {
+                        Memory.upgradersCount = Memory.upgradersCount + 1;
+                        spawn.createCreep([WORK, CARRY, MOVE], 'Upgrader' + Memory.upgradersCount);
+                    }
+                }
+            }
+        }
     }
 };
