68106a93377fbb1876200ec524266cee2f1b114f
FusionLord <FusionLord@live.com>
2016-04-13 13:21:38 -0700

    updated

--- scripts/spawn.js
+++ scripts/spawn.js
@@ -1,4 +1,8 @@
 Spawn.prototype.createDrone = function (type) {
+    if (!this.memory.droneCount)
+    {
+        this.memory.droneCount = -1;
+    }
     this.memory.droneCount = this.memory.droneCount + 1;
     return this.createCreep([WORK, CARRY, MOVE], "Drone" + this.memory.droneCount, {role: type})
 };
